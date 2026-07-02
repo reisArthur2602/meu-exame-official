e90162268efe2034ce653ba32a6884e079fc7b0e
dev.arthur <arthurdesouzareis222@gmail.com>
2026-07-02 08:02:53 -0300

    feat

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  apps: [
-    {
-      name: "meu-exame",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
-      cwd: __dirname,
-      exec_mode: "fork",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "production",
-        PORT: 3000,
-      },
-    },
-  ],
+    apps: [
+        {
+            name: 'meu-exame',
+            script: 'node_modules/next/dist/bin/next',
+            args: 'start',
+            cwd: __dirname,
+            exec_mode: 'fork',
+            instances: 1,
+
+            env: {
+                NODE_ENV: 'production',
+                PORT: 8000,
+            },
+        },
+    ],
 };
